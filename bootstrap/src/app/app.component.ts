55774b645f26cf5a03f27ef7ffed55bd3954fadf
RafaelMarquesSousa <rs102862@alunos.unisanta.br>
2024-01-15 17:30:39 -0300

    fotos

--- bootstrap/src/app/app.component.ts
+++ bootstrap/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PrimeiroComponentComponent } from "./components/primeiro-component/primeiro-component.component";
+
+import { SegundoComponentComponent } from "./components/segundo-component/segundo-component.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, PrimeiroComponentComponent, SegundoComponentComponent,]
 })
 export class AppComponent {
   title = 'bootstrap';
---
+++ bootstrap/src/app/components/segundo-component/segundo-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './segundo-component.component.html',
+  styleUrl: './segundo-component.component.css'
+})
+export class SegundoComponentComponent {
+
+}
